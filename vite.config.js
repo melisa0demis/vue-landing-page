e5484122b401890aa7afc5686fd52758d95e2b5e
melisa <melisa.odemis@protalya.com>
2024-06-08 16:38:25 +0300

    chore: Update Vite configuration and footer layout

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/vue-landing-page/",
-    plugins: [vue()],
+  base: "/vue-landing-page/",
+  plugins: [vue()],
+  server: {
+    port: 3000, // Veya kullanmak istediğiniz başka bir port
+  },
 });
